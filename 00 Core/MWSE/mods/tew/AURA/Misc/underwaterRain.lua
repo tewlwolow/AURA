36c77b1aaab5b83ed4ef0609b774bddb622f79a3
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2023-06-22 01:12:48 +0200

    UNDERWATERRAIN: Don't hardcode values

--- 00 Core/MWSE/mods/tew/AURA/Misc/underwaterRain.lua
+++ 00 Core/MWSE/mods/tew/AURA/Misc/underwaterRain.lua
@@ -2,26 +2,26 @@ local common = require("tew.AURA.common")
 local debugLog = common.debugLog
 
 local weatherSounds = {
-	"Rain",
-	"rain heavy",
-	"Blight",
-	"Ashstorm",
-	"BM Blizzard",
-	"tew_b_rainlight",
-	"tew_b_rainmedium",
-	"tew_b_rainheavy",
-	"tew_s_rainlight",
-	"tew_s_rainmedium",
-	"tew_s_rainheavy",
-	"tew_t_rainlight",
-	"tew_t_rainmedium",
-	"tew_t_rainheavy",
-	"tew_rain_light",
-	"tew_rain_medium",
-	"tew_rain_heavy",
-	"tew_thunder_light",
-	"tew_thunder_medium",
-	"tew_thunder_heavy"
+	["Rain"] = 0,
+	["rain heavy"] = 0,
+	["Blight"] = 0,
+	["Ashstorm" ]= 0,
+	["BM Blizzard"] = 0,
+	["tew_b_rainlight"] = 0,
+	["tew_b_rainmedium"] = 0,
+	["tew_b_rainheavy"] = 0,
+	["tew_s_rainlight"] = 0,
+	["tew_s_rainmedium"] = 0,
+	["tew_s_rainheavy"] = 0,
+	["tew_t_rainlight"] = 0,
+	["tew_t_rainmedium"] = 0,
+	["tew_t_rainheavy"] = 0,
+	["tew_rain_light"] = 0,
+	["tew_rain_medium"] = 0,
+	["tew_rain_heavy"] = 0,
+	["tew_thunder_light"] = 0,
+	["tew_thunder_medium"] = 0,
+	["tew_thunder_heavy"] = 0
 }
 
 local function setVolume(track, volume)
@@ -34,14 +34,15 @@ local function modifyVolume()
 	if not tes3.player or not tes3.mobilePlayer then return end
 	local waterLevel = tes3.player.cell.waterLevel or 0
 	local playerPosZ = tes3.player.position.z
-	for _, id in ipairs(weatherSounds) do
+	for id, _ in pairs(weatherSounds) do
 		if playerPosZ < waterLevel and tes3.getSound(id):isPlaying() then
 			local sound = tes3.getSound(id)
-			local volume = math.clamp(1 - math.remap(waterLevel - playerPosZ, 0, 1500, 0, 1), 0.0, 1.0)
+			local originalVol = weatherSounds[id]
+			local volume = math.clamp(1 - math.remap(waterLevel - playerPosZ, 0, 1500, 0, originalVol), 0.0, originalVol)
 			setVolume(sound, volume)
 		else
 			local sound = tes3.getSound(id)
-			setVolume(sound, 1.0)
+			setVolume(sound, weatherSounds[id] )
 		end
 	end
 end
@@ -64,6 +65,14 @@ local function underWaterCheck(e)
 	end
 end
 
+local function setOriginalVolume()
+	for id, _ in pairs(weatherSounds) do
+		local sound = tes3.getSound(id)
+		local originalVol = sound.volume
+		weatherSounds[id] = originalVol
+	end
+end
+
 local function registerModify()
 	event.unregister(tes3.event.simulate, underWaterCheck)
 	event.unregister(tes3.event.simulate, modifyVolume)
@@ -82,3 +91,5 @@ event.register(tes3.event.simulate, underWaterCheck)
 
 event.register("AURA:enteredUnderwater", registerModify)
 event.register("AURA:exitedUnderwater", unRegisterModify)
+
+event.register(tes3.event.load, setOriginalVolume)

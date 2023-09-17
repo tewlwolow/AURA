74cf0ed732e173f83c62ba370b3e81f5e67a4a3c
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2023-09-17 22:27:34 +0200

    Experimental ChatGPT refactor

--- 00 Core/MWSE/mods/tew/AURA/Service Voices/serviceVoicesMain.lua
+++ 00 Core/MWSE/mods/tew/AURA/Service Voices/serviceVoicesMain.lua
@@ -1,8 +1,8 @@
 local serviceVoicesData = require("tew.AURA.Service Voices.serviceVoicesData")
 local config = require("tew.AURA.config")
 local common = require("tew.AURA.common")
-local UIvol = config.volumes.misc.UIvol / 100
-local SVvol = config.volumes.misc.SVvol / 100
+
+local UIvol, SVvol = config.volumes.misc.UIvol / 100, config.volumes.misc.SVvol / 100
 local moduleUI = config.moduleUI
 
 local raceNames = serviceVoicesData.raceNames
@@ -12,391 +12,98 @@ local spellVoices = serviceVoicesData.spellVoices
 local trainingVoices = serviceVoicesData.trainingVoices
 
 local UISpells = config.UISpells
+local serviceFlags = {
+    serviceRepair = config.serviceRepair,
+    serviceSpells = config.serviceSpells,
+    serviceTraining = config.serviceTraining,
+    serviceSpellmaking = config.serviceSpellmaking,
+    serviceEnchantment = config.serviceEnchantment,
+    serviceTravel = config.serviceTravel,
+    serviceBarter = config.serviceBarter
+}
 
-local serviceRepair = config.serviceRepair
-local serviceSpells = config.serviceSpells
-local serviceTraining = config.serviceTraining
-local serviceSpellmaking = config.serviceSpellmaking
-local serviceEnchantment = config.serviceEnchantment
-local serviceTravel = config.serviceTravel
-local serviceBarter = config.serviceBarter
+local flags = {
+    trainingFlag = 0,
+    spellsFlag = 0,
+    spellMakingFlag = 0,
+    repairFlag = 0
+}
 
-local trainingFlag, spellsFlag, spellMakingFlag, repairFlag = 0, 0, 0, 0
 local newVoice, lastVoice = "init", "init"
 
 local debugLog = common.debugLog
 
-local function serviceGreet(e)
+local function getServiceVoiceData(e, voiceData)
+    local npcId = tes3ui.getServiceActor(e)
+    local raceId = npcId.object.race.id
+    local raceLet = raceNames[raceId]
+    local sexLet = npcId.object.female and "f" or "m"
 
-   local npcId = tes3ui.getServiceActor(e)
-   local raceId = npcId.object.race.id
-   local raceLet, sexLet
-   local serviceFeed = {}
-
-   if npcId.object.female then
-      debugLog("Female NPC found.")
-      sexLet = "f"
-   else
-      sexLet = "m"
-      debugLog("Male NPC found.")
-   end
-
-   for k, v in pairs(raceNames) do
-      if raceId == k then
-         raceLet = v
-      end
-   end
-
-   for kRace, _ in pairs(commonVoices) do
-      if kRace == raceLet then
-         for kSex, _ in pairs(commonVoices[kRace]) do
-            if kSex == sexLet then
-               for _, voice in pairs(commonVoices[kRace][kSex]) do
-                  table.insert(serviceFeed, voice)
-               end
-            end
-         end
-      end
-   end
-
-   if serviceFeed[1] then
-      while newVoice == lastVoice or newVoice == nil do
-         newVoice = serviceFeed[math.random(1, #serviceFeed)]
-      end
-      tes3.removeSound { reference = npcId }
-      tes3.say {
-         volume = 0.9 * SVvol,
-         soundPath = "Vo\\" .. raceLet .. "\\" .. sexLet .. "\\" ..
-             newVoice .. ".mp3", reference = npcId
-      }
-      lastVoice = newVoice
-      debugLog("NPC says a service comment.")
-   end
-
+    return voiceData[raceLet] and voiceData[raceLet][sexLet]
 end
 
-local function travelGreet(e)
+local function playServiceVoice(npcId, raceLet, sexLet, serviceFeed)
+    if #serviceFeed > 0 then
+        while newVoice == lastVoice or newVoice == nil do
+            newVoice = serviceFeed[math.random(1, #serviceFeed)]
+        end
 
-   local npcId = tes3ui.getServiceActor(e)
-   local raceId = npcId.object.race.id
-   local raceLet, sexLet
-   local serviceFeed = {}
-
-   if npcId.object.female then
-      debugLog("Female NPC found.")
-      sexLet = "f"
-   else
-      sexLet = "m"
-      debugLog("Male NPC found.")
-   end
-
-   for k, v in pairs(raceNames) do
-      if raceId == k then
-         raceLet = v
-      end
-   end
-
-   for kRace, _ in pairs(travelVoices) do
-      if kRace == raceLet then
-         for kSex, _ in pairs(travelVoices[kRace]) do
-            if kSex == sexLet then
-               for _, voice in pairs(travelVoices[kRace][kSex]) do
-                  table.insert(serviceFeed, voice)
-               end
-            end
-         end
-      end
-   end
-
-   if serviceFeed[1] then
-      while newVoice == lastVoice or newVoice == nil do
-         newVoice = serviceFeed[math.random(1, #serviceFeed)]
-      end
-      tes3.removeSound { reference = npcId }
-      tes3.say {
-         volume = 0.9 * SVvol,
-         soundPath = "Vo\\" .. raceLet .. "\\" .. sexLet .. "\\" ..
-             newVoice .. ".mp3", reference = npcId
-      }
-      lastVoice = newVoice
-      debugLog("NPC says a travel comment.")
-   end
-
+        tes3.removeSound { reference = npcId }
+        tes3.say {
+            volume = 0.9 * SVvol,
+            soundPath = string.format("Vo\\%s\\%s\\%s.mp3", raceLet, sexLet, newVoice),
+            reference = npcId
+        }
+        lastVoice = newVoice
+        debugLog("NPC says a comment for the service.")
+    end
 end
 
-local function trainingGreet(e)
+local function handleServiceGreet(e, voiceData, flag, closeButtonName, playMysticGateSound, playMenuClickSound)
+    local closeButton = e.element:findChild(tes3ui.registerID(closeButtonName))
+	if closeButton then
+		closeButton:register("mouseDown", function()
+			flags[flag] = 0
+			if playMenuClickSound then
+				tes3.playSound { sound = "Menu Click", reference = tes3.player }
+			end
+		end)
+	end
 
-   local closeButton = e.element:findChild(tes3ui.registerID("MenuServiceTraining_Okbutton"))
-   closeButton:register("mouseDown", function()
-      tes3.playSound { sound = "Menu Click", reference = tes3.player }
-      trainingFlag = 0
-   end)
+    if flags[flag] == 1 then
+        return
+    end
 
-   if trainingFlag == 1 then return end
+    local npcId = tes3ui.getServiceActor(e)
+    local raceId = npcId.object.race.id
+    local raceLet = raceNames[raceId]
+    local sexLet = npcId.object.female and "f" or "m"
 
-   local npcId = tes3ui.getServiceActor(e)
-   local raceId = npcId.object.race.id
-   local raceLet, sexLet
-   local serviceFeed = {}
+    local serviceFeed = getServiceVoiceData(e, voiceData) or {}
 
-   if npcId.object.female then
-      debugLog("Female NPC found.")
-      sexLet = "f"
-   else
-      sexLet = "m"
-      debugLog("Male NPC found.")
-   end
+    playServiceVoice(npcId, raceLet, sexLet, serviceFeed)
 
-   for k, v in pairs(raceNames) do
-      if raceId == k then
-         raceLet = v
-      end
-   end
+    flags[flag] = 1
+    debugLog("NPC says a comment for the service.")
 
-   for kRace, _ in pairs(trainingVoices) do
-      if kRace == raceLet then
-         for kSex, _ in pairs(trainingVoices[kRace]) do
-            if kSex == sexLet then
-               for _, voice in pairs(trainingVoices[kRace][kSex]) do
-                  table.insert(serviceFeed, voice)
-               end
-            end
-         end
-      end
-   end
-
-   if serviceFeed[1] then
-      while newVoice == lastVoice or newVoice == nil do
-         newVoice = serviceFeed[math.random(1, #serviceFeed)]
-      end
-      tes3.removeSound { reference = npcId }
-      tes3.say {
-         volume = 0.9 * SVvol,
-         soundPath = "Vo\\" .. raceLet .. "\\" .. sexLet .. "\\" ..
-             newVoice .. ".mp3", reference = npcId
-      }
-      lastVoice = newVoice
-      debugLog("NPC says a trainer comment.")
-      trainingFlag = 1
-   end
-
+    if playMysticGateSound and UISpells and moduleUI then
+        tes3.playSound { soundPath = "FX\\MysticGate.wav", reference = tes3.player, volume = 0.2 * UIvol, pitch = 1.8 }
+        debugLog("Opening spell menu sound played.")
+    end
 end
 
-local function spellGreet(e)
-
-   local closeButton = e.element:findChild(tes3ui.registerID("MenuServiceSpells_Okbutton"))
-   closeButton:register("mouseDown", function()
-      tes3.playSound { sound = "Menu Click", reference = tes3.player }
-      spellsFlag = 0
-   end)
-
-   if spellsFlag == 1 then return end
-
-   local npcId = tes3ui.getServiceActor(e)
-   local raceId = npcId.object.race.id
-   local raceLet, sexLet
-   local serviceFeed = {}
-
-   if npcId.object.female then
-      debugLog("Female NPC found.")
-      sexLet = "f"
-   else
-      sexLet = "m"
-      debugLog("Male NPC found.")
-   end
-
-   for k, v in pairs(raceNames) do
-      if raceId == k then
-         raceLet = v
-      end
-   end
-
-   for kRace, _ in pairs(spellVoices) do
-      if kRace == raceLet then
-         for kSex, _ in pairs(spellVoices[kRace]) do
-            if kSex == sexLet then
-               for _, voice in pairs(spellVoices[kRace][kSex]) do
-                  table.insert(serviceFeed, voice)
-               end
-            end
-         end
-      end
-   end
-
-   if not serviceFeed[1] then
-      for kRace, _ in pairs(commonVoices) do
-         if kRace == raceLet then
-            for kSex, _ in pairs(commonVoices[kRace]) do
-               if kSex == sexLet then
-                  for _, voice in pairs(commonVoices[kRace][kSex]) do
-                     table.insert(serviceFeed, voice)
-                  end
-               end
-            end
-         end
-      end
-   end
-
-   while newVoice == lastVoice or newVoice == nil do
-      newVoice = serviceFeed[math.random(1, #serviceFeed)]
-   end
-   tes3.removeSound { reference = npcId }
-   tes3.say {
-      volume = 0.9 * SVvol,
-      soundPath = "Vo\\" .. raceLet .. "\\" .. sexLet .. "\\" ..
-          newVoice .. ".mp3", reference = npcId
-   }
-   lastVoice = newVoice
-   spellsFlag = 1
-   debugLog("NPC says a spell vendor comment.")
-
-   if UISpells and moduleUI then
-      tes3.playSound { soundPath = "FX\\MysticGate.wav", reference = tes3.player, volume = 0.2 * UIvol, pitch = 1.8 }
-      debugLog("Opening spell menu sound played.")
-   end
+local function registerGreetEvent(serviceFlag, greetFunction, filter, closeButtonName, playMysticGateSound, playMenuClickSound)
+    if serviceFlags[serviceFlag] then
+        event.register("uiActivated", function(e)
+            handleServiceGreet(e, greetFunction, serviceFlag, closeButtonName, playMysticGateSound, playMenuClickSound)
+        end, { filter = filter, priority = -10 })
+    end
 end
 
-local function spellMakingGreet(e)
-
-   local cancelButton = e.element:findChild(tes3ui.registerID("MenuSpellmaking_Cancelbutton"))
-   cancelButton:register("mouseDown", function()
-      tes3.playSound { sound = "Menu Click", reference = tes3.player }
-      spellMakingFlag = 0
-   end)
-
-   local buyButton = e.element:findChild(tes3ui.registerID("MenuSpellmaking_Buybutton"))
-   buyButton:register("mouseDown", function()
-      spellMakingFlag = 0
-   end)
-
-   if spellMakingFlag == 1 then return end
-
-   local npcId = tes3ui.getServiceActor(e)
-   local raceId = npcId.object.race.id
-   local raceLet, sexLet
-   local serviceFeed = {}
-
-   if npcId.object.female then
-      debugLog("Female NPC found.")
-      sexLet = "f"
-   else
-      sexLet = "m"
-      debugLog("Male NPC found.")
-   end
-
-   for k, v in pairs(raceNames) do
-      if raceId == k then
-         raceLet = v
-      end
-   end
-
-   for kRace, _ in pairs(spellVoices) do
-      if kRace == raceLet then
-         for kSex, _ in pairs(spellVoices[kRace]) do
-            if kSex == sexLet then
-               for _, voice in pairs(spellVoices[kRace][kSex]) do
-                  table.insert(serviceFeed, voice)
-               end
-            end
-         end
-      end
-   end
-
-   if not serviceFeed[1] then
-      for kRace, _ in pairs(commonVoices) do
-         if kRace == raceLet then
-            for kSex, _ in pairs(commonVoices[kRace]) do
-               if kSex == sexLet then
-                  for _, voice in pairs(commonVoices[kRace][kSex]) do
-                     table.insert(serviceFeed, voice)
-                  end
-               end
-            end
-         end
-      end
-   end
-
-   while newVoice == lastVoice or newVoice == nil do
-      newVoice = serviceFeed[math.random(1, #serviceFeed)]
-   end
-   tes3.removeSound { reference = npcId }
-   tes3.say {
-      volume = 0.9 * SVvol,
-      soundPath = "Vo\\" .. raceLet .. "\\" .. sexLet .. "\\" ..
-          newVoice .. ".mp3", reference = npcId
-   }
-   lastVoice = newVoice
-   spellMakingFlag = 1
-   debugLog("NPC says a spellmaking comment.")
-
-   if UISpells and moduleUI then
-      tes3.playSound { soundPath = "FX\\MysticGate.wav", reference = tes3.player, volume = 0.6 * UIvol, pitch = 0.9 }
-      debugLog("Opening spell menu sound played.")
-   end
-
-end
-
-local function repairGreet(e)
-
-   local closeButton = e.element:findChild(tes3ui.registerID("MenuServiceRepair_Okbutton"))
-   closeButton:register("mouseDown", function()
-      repairFlag = 0
-   end)
-
-   if repairFlag == 1 then return end
-
-   local npcId = tes3ui.getServiceActor(e)
-   local raceId = npcId.object.race.id
-   local raceLet, sexLet
-   local serviceFeed = {}
-
-   if npcId.object.female then
-      debugLog("Female NPC found.")
-      sexLet = "f"
-   else
-      sexLet = "m"
-      debugLog("Male NPC found.")
-   end
-
-   for k, v in pairs(raceNames) do
-      if raceId == k then
-         raceLet = v
-      end
-   end
-
-   for kRace, _ in pairs(commonVoices) do
-      if kRace == raceLet then
-         for kSex, _ in pairs(commonVoices[kRace]) do
-            if kSex == sexLet then
-               for _, voice in pairs(commonVoices[kRace][kSex]) do
-                  table.insert(serviceFeed, voice)
-               end
-            end
-         end
-      end
-   end
-
-   if serviceFeed[1] then
-      while newVoice == lastVoice or newVoice == nil do
-         newVoice = serviceFeed[math.random(1, #serviceFeed)]
-      end
-      tes3.removeSound { reference = npcId }
-      tes3.say {
-         volume = 0.9 * SVvol,
-         soundPath = "Vo\\" .. raceLet .. "\\" .. sexLet .. "\\" ..
-             newVoice .. ".mp3", reference = npcId
-      }
-      lastVoice = newVoice
-      repairFlag = 1
-      debugLog("NPC says a repair comment.")
-   end
-
-end
-
-if serviceTravel then event.register("uiActivated", travelGreet, { filter = "MenuServiceTravel", priority = -10 }) end
-if serviceBarter then event.register("uiActivated", serviceGreet, { filter = "MenuBarter", priority = -10 }) end
-if serviceTraining then event.register("uiActivated", trainingGreet, { filter = "MenuServiceTraining", priority = -10 }) end
-if serviceEnchantment then event.register("uiActivated", serviceGreet, { filter = "MenuEnchantment", priority = -10 }) end
-if serviceSpellmaking then event.register("uiActivated", spellMakingGreet, { filter = "MenuSpellmaking", priority = -10 }) end
-if serviceSpells then event.register("uiActivated", spellGreet, { filter = "MenuServiceSpells", priority = -10 }) end
-if serviceRepair then event.register("uiActivated", repairGreet, { filter = "MenuServiceRepair", priority = -10 }) end
+registerGreetEvent("serviceTravel", travelVoices, "MenuServiceTravel", "", false, true) -- Play Menu Click
+registerGreetEvent("serviceBarter", commonVoices, "MenuBarter", "", false, true) -- Play Menu Click
+registerGreetEvent("serviceTraining", trainingVoices, "MenuServiceTraining", "MenuServiceTraining_Okbutton", false, true) -- Play Menu Click
+registerGreetEvent("serviceEnchantment", commonVoices, "MenuEnchantment", "", false, true) -- Play Menu Click
+registerGreetEvent("serviceSpellmaking", spellVoices, "MenuSpellmaking", "MenuSpellmaking_Cancelbutton", false, true) -- Play Menu Click
+registerGreetEvent("serviceSpells", spellVoices, "MenuServiceSpells", "MenuServiceSpells_Okbutton", true, true) -- Play both Mystic Gate and Menu Click
+registerGreetEvent("serviceRepair", commonVoices, "MenuServiceRepair", "MenuServiceRepair_Okbutton", false, true) -- Play Menu Click

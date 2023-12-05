1cb46b754d9a6d6b9872689aacef529456c057e4
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2023-12-05 01:52:18 +0100

    FLORA: Added some OAAB and TR ids

--- 00 Core/MWSE/mods/tew/AURA/Misc/floraSounds.lua
+++ 00 Core/MWSE/mods/tew/AURA/Misc/floraSounds.lua
@@ -37,12 +37,61 @@ local ids = {
     "flora_stoneflower",
     "flora_wickwheat",
     "flora_willow_flower",
+    "AB_Flora_AcSapling01",
+    "AB_Flora_AcSapling02",
+    "AB_Flora_AcSapling03",
+    "AB_Flora_AcSapling04",
+    "AB_Flora_AiSapling01",
+    "AB_Flora_AiSapling02",
+    "AB_Flora_AiSapling03",
+    "AB_Flora_AiSapling04",
+    "AB_Flora_BcSapling01",
+    "AB_Flora_BcSapling02",
+    "AB_Flora_BcSapling03",
+    "AB_Flora_GlSapling01",
+    "AB_Flora_GlSapling02",
+    "AB_Flora_WgSapling01",
+    "AB_Flora_WgSapling02",
+    "AB_Flora_WgSapling03",
+    "AB_Flora_WgSapling04",
+    "AB_Flora_GoldReed01",
+    "AB_Flora_GoldReed02",
+    "AB_Flora_GoldReed03",
+    "AB_Flora_GoldReed04",
+    "AB_Flora_GoldReedGrass",
+    "AB_Flora_GoldReedGroup01",
+    "AB_Flora_GoldReedGroup02",
+    "AB_Flora_GoldReedGroup03",
+    "AB_Flora_MvGrass_01",
+    "AB_f_Bloodgrass_01",
+    "AB_f_Bloodgrass_02",
+    "AB_f_BlueKanet_01",
+    "AB_f_BlueKanet_02",
+    "AB_f_HackleLo3_o",
+    "AB_f_HackleLo4_o",
+    "AB_f_HackleLo5_o",
+    "AB_f_HackleLo6_o",
+    "AB_f_PomegranateTree01",
+    "AB_f_PomegranateTree02",
+    "T_Bkm_Flora_SomnFern01",
+    "T_Cyr_Flora_Alkanet01",
+    "T_Cyr_Flora_Chokeberr01",
+    "T_Cyr_Flora_Monkshood01",
+    "T_Cyr_Flora_MorningGl01",
+    "T_Cyr_Flora_MorningGl02",
+    "T_Cyr_Flora_MorningGl03",
+    "T_Cyr_Flora_Motherw01",
+    "T_Cyr_Flora_Peony01",
+    "T_Cyr_Flora_Peony02",
+    "T_Cyr_Flora_PoisonLil01",
+    "T_Cyr_Flora_Primrose01",
+    "T_Cyr_Flora_Primrose02",
+    "T_Cyr_Flora_Primrose03",
 }
 
 local lastPlayerPos
 
 local function playFlora()
-
     if not tes3.mobilePlayer then return end
 
     local player = tes3.mobilePlayer.reference
@@ -58,16 +107,16 @@ local function playFlora()
     end
 
     local playerHeight = tes3.player.object.boundingBox and tes3.player.object.boundingBox.max.z or 0
-    local hitResult = tes3.rayTest{
+    local hitResult = tes3.rayTest {
         position = {
             player.position.x,
             player.position.y,
-            player.position.z + (playerHeight / 2)
+            player.position.z + (playerHeight / 2),
         },
-        direction = {0, 0, -1},
+        direction = { 0, 0, -1 },
         findAll = false,
         maxDistance = (playerHeight / 2) + 50,
-        ignore = {player},
+        ignore = { player },
         useModelCoordinates = true,
         useBackTriangles = false,
     }
@@ -75,10 +124,10 @@ local function playFlora()
     local object = hitResult and hitResult.reference and hitResult.reference.object
 
     if not object
-    or not (object.objectType == tes3.objectType.static
+        or not (object.objectType == tes3.objectType.static
             or object.objectType == tes3.objectType.container)
-    or not common.getMatch(ids, object.id:lower())
-    or (object.deleted or object.disabled) then
+        or not common.getMatch(ids, object.id:lower())
+        or (object.deleted or object.disabled) then
         return
     end
 
@@ -89,14 +138,14 @@ local function playFlora()
     local volume = config.volumes.misc.floraVol / 100
     local pitch = math.random(80, 130) / 100
 
-    tes3.playSound{
+    tes3.playSound {
         sound = sound,
         reference = hitResult.reference,
         volume = volume,
         pitch = pitch,
         loop = false,
     }
-    debugLog('Played ' .. sound.id)
+    debugLog("Played " .. sound.id)
     lastPlayerPos = playerPos:copy()
 end
 

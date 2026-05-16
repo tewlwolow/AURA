237c53b8188d118ce8990d1c24582c5628c5a9bd
tewlwolow <leonczernecki@gmail.com>
2026-05-16 17:40:24 +0200

    Anti-repeat

--- 00 Core/MWSE/mods/tew/AURA/PC/taunts.lua
+++ 00 Core/MWSE/mods/tew/AURA/PC/taunts.lua
@@ -5,9 +5,48 @@ local raceNames = serviceVoicesData.raceNames
 local tauntsData = require("tew.AURA.PC.tauntsData")
 local common = require("tew.AURA.common")
 local playedTaunt = 0
-local werewolfSounds = { "were moan", "were roar", "were scream", "weregrowl", "werehowl" }
+local lastTaunt
+
+local werewolfSounds = {
+    "were moan",
+    "were roar",
+    "were scream",
+    "weregrowl",
+    "werehowl",
+}
 
 local debugLog = common.debugLog
+
+local function getRandomTaunt(taunts)
+    if not taunts or #taunts == 0 then
+        return nil
+    end
+
+    -- Only one possible taunt.
+    if #taunts == 1 then
+        lastTaunt = taunts[1]
+        return taunts[1]
+    end
+
+    local taunt
+
+    repeat
+        taunt = taunts[math.random(1, #taunts)]
+    until taunt ~= lastTaunt
+
+    lastTaunt = taunt
+
+    return taunt
+end
+
+local function getNonRepeatingTaunt(preferredTaunt, fallbackTaunts)
+    if preferredTaunt ~= lastTaunt then
+        lastTaunt = preferredTaunt
+        return preferredTaunt
+    end
+
+    return getRandomTaunt(fallbackTaunts)
+end
 
 --[[local function getArrays()
 
@@ -64,15 +103,14 @@ local debugLog = common.debugLog
 
 end]]
 
-
 local function playerCheck()
     playedTaunt = 0
 
     playerSex = tes3.player.object.female and "f" or "m"
     playerRace = raceNames[tes3.player.object.race.id]
 
-    debugLog("Determined player race: " .. playerRace)
-    debugLog("Determined player sex: " .. playerSex)
+    debugLog("Determined player race: " .. tostring(playerRace))
+    debugLog("Determined player sex: " .. tostring(playerSex))
 end
 
 local function onStatReview(e)
@@ -85,83 +123,116 @@ end
 
 ---@param e attackEventData
 local function combatCheck(e)
-    if (e.reference ~= tes3.player) then
+    if e.reference ~= tes3.player then
         return
     end
 
-    if tes3.mobilePlayer and tes3.mobilePlayer.isDead then return end
+    if tes3.mobilePlayer and tes3.mobilePlayer.isDead then
+        return
+    end
 
     if playedTaunt == 1 then
         debugLog("Flag on. Returning.")
         return
     end
 
-    if not e.targetMobile then return end
+    if not e.targetMobile then
+        return
+    end
 
     local playerMobile = tes3.mobilePlayer
-
-    local targetMobile = e.targetMobile
     local targetRef = e.targetReference
 
     if tes3.mobilePlayer.werewolf then
-        local taunt = werewolfSounds[math.random(1, #werewolfSounds)]
+        local taunt = getRandomTaunt(werewolfSounds)
+
         tes3.playSound {
             sound = taunt,
             volume = 0.9 * (config.volumes.misc.tVol / 100),
             reference = playerMobile.reference,
         }
+
         playedTaunt = 1
+
         debugLog("Played werewolf battle taunt: " .. taunt)
 
-        timer.start { type = timer.real, duration = 7, callback = function()
-            playedTaunt = 0
-        end }
-    elseif
-        playerRace ~= nil
-        and playerSex ~= nil then
-        if config.tauntChance < math.random() then
-            debugLog("Dice roll failed. Returning.")
-            return
-        end
-
-        local taunt
-
-        if targetRef.object.objectType == tes3.objectType.npc then
-            local foeRace = targetRef.object.race.id
-            debugLog("Foe race: " .. foeRace)
-            local raceTaunts = tauntsData.raceTaunts
-            if raceTaunts[playerRace]
-                and raceTaunts[playerRace][playerSex]
-                and raceTaunts[playerRace][playerSex][foeRace] then
-                taunt = raceTaunts[playerRace][playerSex][foeRace]
-            end
-            if taunt ~= nil then
-                debugLog("Race-based taunt: " .. taunt)
-            end
-            if taunt == nil then
-                local taunts = tauntsData.NPCtaunts
-                taunt = taunts[playerRace][playerSex][math.random(1, #taunts[playerRace][playerSex])]
-                debugLog("NPC taunt: " .. taunt)
-            end
-        else
-            local taunts = tauntsData.Crtaunts
-            taunt = taunts[playerRace][playerSex][math.random(1, #taunts[playerRace][playerSex])]
-            debugLog("Creature taunt: " .. taunt)
-        end
-
-        tes3.say {
-            volume = 0.9 * (config.volumes.misc.tVol / 100),
-            soundPath = "Vo\\" .. playerRace .. "\\" .. playerSex .. "\\" .. taunt,
-            reference = playerMobile.reference,
+        timer.start {
+            type = timer.real,
+            duration = 7,
+            callback = function()
+                playedTaunt = 0
+            end,
         }
 
-        playedTaunt = 1
-        debugLog("Played battle taunt: " .. taunt)
-
-        timer.start { type = timer.real, duration = 6, callback = function()
-            playedTaunt = 0
-        end }
+        return
     end
+
+    if playerRace == nil or playerSex == nil then
+        return
+    end
+
+    if config.tauntChance < math.random() then
+        debugLog("Dice roll failed. Returning.")
+        return
+    end
+
+    local taunt
+
+    if targetRef.object.objectType == tes3.objectType.npc then
+        local foeRace = targetRef.object.race.id
+
+        debugLog("Foe race: " .. tostring(foeRace))
+
+        local raceTaunts = tauntsData.raceTaunts
+
+        if raceTaunts[playerRace]
+            and raceTaunts[playerRace][playerSex]
+            and raceTaunts[playerRace][playerSex][foeRace] then
+            taunt = getNonRepeatingTaunt(
+                raceTaunts[playerRace][playerSex][foeRace],
+                tauntsData.NPCtaunts[playerRace][playerSex]
+            )
+
+            debugLog("Race-based taunt: " .. taunt)
+        end
+
+        if taunt == nil then
+            local taunts = tauntsData.NPCtaunts
+
+            taunt = getRandomTaunt(taunts[playerRace][playerSex])
+
+            debugLog("NPC taunt: " .. taunt)
+        end
+    else
+        local taunts = tauntsData.Crtaunts
+
+        taunt = getRandomTaunt(taunts[playerRace][playerSex])
+
+        debugLog("Creature taunt: " .. taunt)
+    end
+
+    if not taunt then
+        debugLog("No taunt found. Returning.")
+        return
+    end
+
+    tes3.say {
+        volume = 0.9 * (config.volumes.misc.tVol / 100),
+        soundPath = "Vo\\" .. playerRace .. "\\" .. playerSex .. "\\" .. taunt,
+        reference = playerMobile.reference,
+    }
+
+    playedTaunt = 1
+
+    debugLog("Played battle taunt: " .. taunt)
+
+    timer.start {
+        type = timer.real,
+        duration = 6,
+        callback = function()
+            playedTaunt = 0
+        end,
+    }
 end
 
 event.register("loaded", playerCheck)
